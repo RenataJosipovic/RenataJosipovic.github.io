ea528e08eb0e9b99c749336cefb10a75487034e8
Renata <91370601+RenataJosipovic@users.noreply.github.com>
2023-11-23 17:02:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,44 @@
-
 document.addEventListener('DOMContentLoaded', function () {
-    let currentSlide = 0;
-    const slides = document.querySelectorAll('.carousel-slide');
-    const totalSlides = slides.length;
-  
-    function showSlide(index) {
-      slides.forEach((slide, i) => {
-        if (i === index) {
-          slide.style.display = 'block';
-        } else {
-          slide.style.display = 'none';
-        }
-      });
-    }
-  
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % totalSlides;
-      showSlide(currentSlide);
-    }
-  
-    function prevSlide() {
-      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-      showSlide(currentSlide);
-    }
-  
-    // Show the first slide initially
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.carousel-slide');
+  const totalSlides = slides.length;
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      if (i === index) {
+        slide.style.display = 'block';
+      } else {
+        slide.style.display = 'none';
+      }
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
     showSlide(currentSlide);
-  
-    // Event listeners for next and previous buttons
-    document.getElementById('nextBtn').addEventListener('click', nextSlide);
-    document.getElementById('prevBtn').addEventListener('click', prevSlide);
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+    showSlide(currentSlide);
+  }
+
+  // Show the first slide initially
+  showSlide(currentSlide);
+
+  // Event listeners for next and previous buttons
+  document.querySelectorAll('.nextBtn').forEach(btn => {
+    btn.addEventListener('click', nextSlide);
   });
 
-document.addEventListener('DOMContentLoaded', function () {
-    var videoFrame = document.getElementById('videoFrame');
-
-    // Pausa videon när sidan laddas
-    videoFrame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+  document.querySelectorAll('.prevBtn').forEach(btn => {
+    btn.addEventListener('click', prevSlide);
+  });
 });
 
-  
+document.addEventListener('DOMContentLoaded', function () {
+  var videoFrame = document.getElementById('videoFrame');
+
+  // Pausa videon när sidan laddas
+  videoFrame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
+});
