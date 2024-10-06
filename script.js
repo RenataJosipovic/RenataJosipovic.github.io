0a4bd0846d6018cc46056079a5f1a665c5f32513
Renata <91370601+RenataJosipovic@users.noreply.github.com>
2024-10-06 19:20:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const carouselContainers = document.querySelectorAll('.carousel-container');
+    const carouselContainers = document.querySelectorAll('.carousel-container');
 
-  carouselContainers.forEach(container => {
-    const slides = container.querySelectorAll('.carousel-slide');
-    const totalSlides = slides.length;
-    let currentSlide = 0; // Unik currentSlide för varje karusell
+    carouselContainers.forEach(container => {
+        const slides = container.querySelectorAll('.carousel-slide');
+        const totalSlides = slides.length;
+        let currentSlide = 0; // Unik currentSlide för varje karusell
 
-    // Funktion för att visa ett specifikt slide
-    function showSlide(index) {
-      slides.forEach((slide, i) => {
-        slide.style.display = i === index ? 'block' : 'none';
-      });
-    }
+        // Funktion för att visa ett specifikt slide
+        function showSlide(index) {
+            slides.forEach((slide, i) => {
+                slide.style.display = i === index ? 'flex' : 'none';
+            });
+        }
 
-    // Visa första bilden när sidan laddas
-    showSlide(currentSlide);
+        // Visa första bilden när sidan laddas
+        showSlide(currentSlide);
 
-    // Funktion för nästa slide
-    function nextSlide() {
-      currentSlide = (currentSlide + 1) % totalSlides;
-      showSlide(currentSlide);
-    }
+        // Funktion för nästa slide
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % totalSlides;
+            showSlide(currentSlide);
+        }
 
-    // Funktion för föregående slide
-    function prevSlide() {
-      currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
-      showSlide(currentSlide);
-    }
+        // Funktion för föregående slide
+        function prevSlide() {
+            currentSlide = (currentSlide - 1 + totalSlides) % totalSlides;
+            showSlide(currentSlide);
+        }
 
-    // Lägg till event listeners för nästa och föregående knappar
-    container.querySelector('.nextBtn').addEventListener('click', nextSlide);
-    container.querySelector('.prevBtn').addEventListener('click', prevSlide);
-  });
+        // Lägg till event listeners för nästa och föregående knappar
+        container.querySelector('.nextBtn').addEventListener('click', nextSlide);
+        container.querySelector('.prevBtn').addEventListener('click', prevSlide);
+    });
 });
 
-// Script för att pausa videon när sidan laddas
-document.addEventListener('DOMContentLoaded', function () {
-  var videoFrame = document.getElementById('videoFrame');
-
-  if (videoFrame) {
     // Pausa videon när sidan laddas
     videoFrame.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*');
   }
